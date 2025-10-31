12eb8124da6d875ca55641e48d6d04a5320729f6
Kavita Asija <kavitaasija2@gmail.com>
2025-10-31 13:33:25 +0530

    refactor unique key generator

--- src/main/java/org/url/shortener/service/UniqueKeyGenerator.java
+++ src/main/java/org/url/shortener/service/UniqueKeyGenerator.java
@@ -2,14 +2,25 @@ package org.url.shortener.service;
 
 import java.security.SecureRandom;
 
+
 public class UniqueKeyGenerator {
 
-  private final static String ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  private static final String ALLOWED_CHARS = 
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  
+  private final SecureRandom random;
 
-  public String generateUniqueKey(int maxLen) {
-    SecureRandom random = new SecureRandom();
-    StringBuilder uniqueKey = new StringBuilder();
-    for (int i = 0; i < maxLen; i++) {
+  public UniqueKeyGenerator() {
+    this.random = new SecureRandom();
+  }
+
+  public String generateUniqueKey(int length) {
+    if (length <= 0) {
+      throw new IllegalArgumentException("Key length must be positive");
+    }
+    
+    StringBuilder uniqueKey = new StringBuilder(length);
+    for (int i = 0; i < length; i++) {
       uniqueKey.append(ALLOWED_CHARS.charAt(random.nextInt(ALLOWED_CHARS.length())));
     }
     return uniqueKey.toString();
