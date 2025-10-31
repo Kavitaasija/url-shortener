ffe8c728020544e0ef7ec3c2e615901d340afbe0
Kavita Asija <kavitaasija2@gmail.com>
2025-10-31 13:30:22 +0530

    scheduler configs

--- src/main/java/org/url/shortener/scheduler/SchedulerConfig.java
+++ src/main/java/org/url/shortener/scheduler/SchedulerConfig.java
@@ -1,9 +1,26 @@
 package org.url.shortener.scheduler;
 
-public class SchedulerConfig {
-  Integer frequency;
 
-  public SchedulerConfig(Integer frequency) {
+public class SchedulerConfig {
+  private final Integer frequency;
+  private final Integer threadPoolSize;
+
+  public SchedulerConfig(Integer frequency, Integer threadPoolSize) {
+    if (frequency == null || frequency <= 0) {
+      throw new IllegalArgumentException("Frequency must be positive");
+    }
+    if (threadPoolSize == null || threadPoolSize <= 0) {
+      throw new IllegalArgumentException("Thread pool size must be positive");
+    }
     this.frequency = frequency;
+    this.threadPoolSize = threadPoolSize;
+  }
+  
+  public Integer getFrequency() {
+    return frequency;
+  }
+  
+  public Integer getThreadPoolSize() {
+    return threadPoolSize;
   }
 }
